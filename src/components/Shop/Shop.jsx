c869130aefb5b6e2650c93d83f17b59bfa8f8fe9
Auporna-Barua <120823697+Auporna-Barua@users.noreply.github.com>
2023-04-04 12:46:35 +0600

    added cart component and calculation

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import "./Shop.css";
+import Product from "../Product/Product";
+import Cart from "../Cart/Cart";
+
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const handleAddToCart = (product) => {
+    // cart.push(product);
+    const newCart = [...cart, product];
+    setCart(newCart);
+  }
+
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        {products.map((product) => (
+          <Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
